51d20a5f0f8e8df2e3c0e17be15678c030daf9de
sebaisfly <sebagaming624@gmail.com>
2025-11-10 21:28:58 +0300

    home page backend

---
+++ food-system/src/main/java/com/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.food_system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "index"; // loads templates/index.html
+    }
+}
